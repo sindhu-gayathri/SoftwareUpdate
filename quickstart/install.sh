2900241a7d9f09fd9a5dd9caeec34d3f6d58cfaa
SindhuGayathri <gdasindhugayathri@gmail.com>
2024-07-24 12:29:26 +0530

    Update install.sh

--- quickstart/install.sh
+++ quickstart/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/eclipse-kanto/kanto/releases/download/v0.1.0-M3/kanto_0.1.0-M3_linux_x86_64.deb && sudo apt install ./kanto_1.0.0_linux_x86_64.deb
+wget https://github.com/eclipse-kanto/kanto/releases/download/v0.1.0-M3/kanto_0.1.0-M3_linux_x86_64.deb && apt install ./kanto_1.0.0_linux_x86_64.deb
