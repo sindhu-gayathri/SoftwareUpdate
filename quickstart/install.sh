586b1c080036b9fe5864b4ac952714ef496d5b23
SindhuGayathri <gdasindhugayathri@gmail.com>
2024-07-25 11:34:06 +0530

    Update install.sh

--- quickstart/install.sh
+++ quickstart/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/bin/cp -rf /modules/0/kanto_0.1.0-M3_linux_x86_64.deb /home/gayathri/FOTA_Testing
-/usr/bin/chmod +x /home/gayathri/FOTA_Testing/kanto_0.1.0-M3_linux_x86_64.deb
+#/usr/bin/chmod +x /home/gayathri/FOTA_Testing/kanto_0.1.0-M3_linux_x86_64.deb
 cd /home/gayathri/FOTA_Testing/
 apt-get update
 apt install ./kanto_0.1.0-M3_linux_x86_64.deb
