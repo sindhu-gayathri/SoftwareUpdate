62c6fef9ea53b49269a5248d4118330eeb119bc2
SindhuGayathri <gdasindhugayathri@gmail.com>
2024-08-08 18:00:54 +0530

    Create phantom_install.sh

---
+++ quickstart/phantom_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILENAME=""
+FILE_ID=1nieUygVQwF1Ti0VU3g9F0L6XquBscQ1u
+DESTINATION="phantom-rootfsraw-prod-update_01.03.000.swu"
+wget "https://drive.usercontent.google.com/download?id=$FILE_ID&export=download&authuser=1&confirm=t" -O $DESTINATION
+result=$?
+
+FILENAME=$(ls *.swu)
+if [ $result == 0 ]; then
+    swupdate -m -M -i $FILENAME
+    result=$?
+fi
+
+if [ $result == 0 ]; then
+    reboot
+else 
+    echo "SOFTWARE UPDATE ERROR"	
+fi
