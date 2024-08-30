21f45f18fb67246ff831ce36f23253b4a37492c9
SindhuGayathri <gdasindhugayathri@gmail.com>
2024-08-30 10:53:45 +0530

    Create file-upload_install.sh

---
+++ quickstart/file-upload_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/SindhuGayathri/SoftwareUpdate/main/quickstart/file-upload_config.json
+cp -rf file-upload_config.json /etc/file-upload/config.json
+systemctl restart file-upload
+
+sleep 10 
+
+pip3 install servefile 
+servefile -u /var/log/suite-connector/
